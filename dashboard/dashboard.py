1a52d4e098425734ea4087514e0a7b7e2e6a7b51
Dessy Fitriaya Anwari <103984794+dessyfitriaya@users.noreply.github.com>
2024-01-29 08:38:55 +0800

    change all_data.csv path

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -47,7 +47,7 @@ def create_rfm_df(df):
     
     return rfm_df
 
-all_df = pd.read_csv("dashboard/all_data.csv")
+all_df = pd.read_csv("https://raw.githubusercontent.com/dessyfitriaya/ecommerce-public-analysis/main/dashboard/all_data.csv")
 
 datetime_columns = ["order_purchase_timestamp", "order_delivered_customer_date"]
 all_df.sort_values(by="order_purchase_timestamp", inplace=True)
